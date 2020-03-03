3a09fe4903591e723e657df8f012e7d212490046
alexvirga <alexander.virga@gmail.com>
2020-03-03 16:35:22 -0500

    added balance to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_024411) do
+ActiveRecord::Schema.define(version: 2020_03_03_212405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_024411) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "balance", default: 5000
   end
 
 end
