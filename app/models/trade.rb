d95aa4e7516a3cb139a7e1712f94a0039a508615
alexvirga <alexander.virga@gmail.com>
2020-03-08 00:05:15 -0500

    Trade validates if quantity is a whole number & greater than one

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,5 @@
 class Trade < ApplicationRecord
   belongs_to :user
   validates :quantity, numericality: { only_integer: true, message: "must be a whole number" }
+  validates :quantity, numericality: { :greater_than_or_equal_to => 1 }
 end
