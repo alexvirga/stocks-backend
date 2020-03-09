4b425f97034ee7318ec6ba006855129e672fda4f
alexvirga <alexander.virga@gmail.com>
2020-03-09 14:43:37 -0400

    Fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://alexvirga.github.io/Stocks-frontend/"
+    origins "*"
 
     resource "*",
       headers: :any,
